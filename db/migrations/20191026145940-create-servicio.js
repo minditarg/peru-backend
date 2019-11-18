429918a8acdbcef18f4fb4d67f51f67272df635a
Ramiro <ramiii.92@hotmail.com>
2019-11-18 17:42:13 -0300

    rename tabla fix forein key

--- db/migrations/20191026145940-create-servicio.js
+++ db/migrations/20191026145940-create-servicio.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'subcategoria',
+          model: 'Subcategoria',
           Key: 'id',
           as: 'subcategoriaId'
         }
