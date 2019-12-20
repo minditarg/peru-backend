4322d476c79a5ffa001d63eb964a74cbcf5dffe5
Ramiro <ramiii.92@hotmail.com>
2019-12-20 01:54:26 -0300

    lcoalidades tablename

--- db/models/localidades.js
+++ db/models/localidades.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
     },
-  }, {tableName: "localidades"});
+  }, {tableName: "Localidades"});
   Localidad.associate = function(models) {
     // associations can be defined here
   };
